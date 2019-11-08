e64025eb43da472ddf097dc168117d5ff84f2447
Tamás Judit <judit0310@gmail.com>
2019-11-08 11:00:26 +0100

    File writing example

--- core/src/test/java/hu/uni/eszterhazy/PlayingJava.java
+++ core/src/test/java/hu/uni/eszterhazy/PlayingJava.java
@@ -3,6 +3,7 @@ package hu.uni.eszterhazy;
 import org.junit.Test;
 
 import java.io.File;
+import java.io.FileWriter;
 import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
@@ -10,15 +11,24 @@ import java.util.List;
 public class PlayingJava {
     @Test
     public void test() throws IOException {
-      File file = new File("target");
-      if(!file.exists()){
-          file.createNewFile();
-      }
+        File file = new File("target");
+        if (!file.exists()) {
+            file.createNewFile();
+        }
         System.out.println(file.getAbsolutePath());
         List<File> files = Arrays.asList(file.listFiles());
-        for(File f: files){
+        for (File f : files) {
             System.out.println(f.getName());
         }
+
+        File kiscica = new File("kiscica.txt");
+        if (!kiscica.exists()) {
+            kiscica.createNewFile();
+        }
+        FileWriter writer = new FileWriter(kiscica);
+        writer.write("Szia");
+        writer.write("Mizu");
+        writer.close();
 
     }
 }
